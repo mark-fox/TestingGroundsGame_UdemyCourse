e2b83c1933450005ca5f1071d7df8e98a31b1ad3
MF <anemail@compuserv.com>
2018-03-02 14:12:53 -0600

    Removed PatrollingGuard class

--- Source/TestingGroundsUdemy/ChooseNextWaypoint.cpp
+++ Source/TestingGroundsUdemy/ChooseNextWaypoint.cpp
@@ -3,16 +3,21 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"	// TODO remove coupling.
+//#include "PatrollingGuard.h"	// TODO remove coupling.
+#include "PatrolRoute.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	// Get the patrol points.
+	// Get the patrol route.
 	auto AIController = OwnerComp.GetAIOwner();
 	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	// Get the patrol points.
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0) { return EBTNodeResult::Failed; }
 
 	// Set next waypoint.
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
