692c1ec30750aaf336ff65236c77944befa69f21
MF <not@email.com>
2018-03-08 19:41:37 -0600

    Refactored npc and player folders together

--- Source/TestingGroundsUdemy/TestingGroundsUdemyGameMode.cpp
+++ Source/TestingGroundsUdemy/TestingGroundsUdemyGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundsUdemyGameMode::ATestingGroundsUdemyGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Character/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
